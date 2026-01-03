187d534804b32a5cb2ec4ff9a639296c167cc11c
Thiago Silva <thiagompta008@hotmail.com>
2026-01-03 02:46:25 -0300

    Fazendo mudanças para entender melhor

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Concluí a aula 01 e agora estou mergulhando em Java!");
+        System.out.println("Esse é o Screen Match");
+        System.out.println("Filme: Top Gun Maverick");
+
+        int anoDeLancamento = 2022;
+        System.out.println("ano de Lançamento: " + anoDeLancamento);
+        boolean incluidoNoPlano = true;
+        double notaDoFilme = 8.1;
+        //Média calculada pelas 3 notas dos professores da Alura
+        double media = (9.8 + 6.3 + 8.0) / 3;
+        String mediaFormatada = String.format("%.2f", media);
+        System.out.println("Média: " + mediaFormatada);
+        String sinopse;
+        sinopse = """
+                Filme Top Gun
+                Filme de aventura com galã dos anos 80
+                Muito bom!
+                Ano de lançamento
+                """ + anoDeLancamento;
+        System.out.println(sinopse);
+
+        int classificacao = (int) (media /2);
+        System.out.println(classificacao);
+
     }
 }
